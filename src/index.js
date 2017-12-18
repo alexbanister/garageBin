dd6a393c722710b8d740482dd16b9f7aa548ba26
Alex Banister <alexbanister@gmail.com>
2017-12-18 13:01:06 -0700

    display all items

--- src/index.js
+++ src/index.js
@@ -1,4 +1,49 @@
 import $ from 'jquery';
+import {
+  getGarageItems,
+  postGarageItem,
+  patchGarageItem
+} from './api';
 import './styles.scss';
 
-$(document).ready(console.log('working'));
+let allItems = [];
+
+const loadAllItems = async ()=> {
+  allItems = await getGarageItems();
+  allItems.sort((a, b) => {
+    return a.name - b.name;
+  });
+  renderItems(allItems);
+  setCounts();
+};
+
+const renderItems = items => {
+  $('.list-container').html('');
+  const documentFragment = $(document.createDocumentFragment());
+  items.forEach(item => {
+    documentFragment.append(buildDOMItem(item));
+  });
+  $('.list-container').append(documentFragment);
+};
+
+const buildDOMItem = item => {
+  const newItem = $('.one-item').clone(true)
+    .data('id', item.id);
+  $(newItem).find('h3').text(item.name);
+  $(newItem).find('.item-reason').text(item.reason);
+  $(newItem).find('[name="cleanliness"]').val(item.cleanliness);
+  return newItem;
+};
+
+const setCounts = () => {
+  const total = $('.list-container').find('.one-item').length;
+  $('.total-count').text(total);
+  const sparkling = $('.list-container').find('[value="Sparkling"]:selected').length;
+  $('.sparkling-count').text(sparkling);
+  const dusty = $('.list-container').find('[value="Dusty"]:selected').length;
+  $('.dusty-count').text(dusty);
+  const rancid = $('.list-container').find('[value="Rancid"]:selected').length;
+  $('.rancid-count').text(rancid);
+};
+
+$(document).ready(loadAllItems());
